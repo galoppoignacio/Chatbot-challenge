76fc07017c4c9cd2465016190e8e0dfcf25671ef
Igna <97696@sistemas.frc.utn.edu.ar>
2025-07-06 14:53:09 -0300

    update backend

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,11 +1,11 @@
 import * as admin from 'firebase-admin';
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG!);
+
 export const firebaseApp = admin.apps.length
   ? admin.app()
   : admin.initializeApp({
-      credential: admin.credential.cert(
-        require('../../firebase-service-account.json') as admin.ServiceAccount
-      ),
+      credential: admin.credential.cert(serviceAccount),
     });
 
 export const firestore = admin.firestore();
